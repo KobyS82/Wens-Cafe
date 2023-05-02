2568a6c46adbb66ae35d1f1e6bd9d5fe5b007e77
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-05-02 17:31:33 -0500

    added background images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,29 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import beef from "./components/beef-stock-photo.jpg";
+import redBackground from "./components/chinese-red-background.jpg";
+import whiteBackground from "./components/chinese-background.jpg";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-400">
-      Hello!
+    <main className="flex min-h-screen flex-col items-center justify-between bg-slate-400 text-black">
+      <div className="absolute ">
+        <Image src={redBackground} alt="background image" className="w-screen h-fit object-fill"/>
+      </div>
+      <div className="relative z-10 flex flex-col items-center justify-center h-full p-6 shadow">
+        <h1 className="text-2xl font-bold">
+          Next JS 13 Background Image with Tailwind CSS{" "}
+        </h1>
+        <p className="mt-4 text-sm">
+          lorem ipsom Next JS 13 Background Image with Tailwind CSS
+        </p>
+      </div>
+
+      {/* <div className='w-full'>
+        <Image src={beef} alt="beef" width={500} height={100} />
+      </div> */}
     </main>
-  )
+  );
 }
