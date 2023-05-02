f059874eea49c6478824b330b1f3d0d134f3a8ae
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-05-02 18:09:18 -0500

    made background img reactive

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,17 @@ import { Inter } from "next/font/google";
 import beef from "./components/beef-stock-photo.jpg";
 import redBackground from "./components/chinese-red-background.jpg";
 import whiteBackground from "./components/chinese-background.jpg";
+import grayBackground from "./components/gray-background.jpeg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-slate-400 text-black">
-      <div className="absolute ">
-        <Image src={redBackground} alt="background image" className="w-screen h-fit object-fill"/>
+      <div className="absolute hidden md:block">
+        <Image src={grayBackground} alt="background image" className="w-screen h-2/3 object-fill"/>
       </div>
-      <div className="relative z-10 flex flex-col items-center justify-center h-full p-6 shadow">
+      <div className="relative z-10 flex flex-col items-center justify-center h-full px-10 py-6">
         <h1 className="text-2xl font-bold">
           Next JS 13 Background Image with Tailwind CSS{" "}
         </h1>
